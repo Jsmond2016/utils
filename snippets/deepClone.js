1c578a74802e609e5a5fb64a070b40aaf2178266
Jsmond2016 <Jsmond2016@gmail.com>
2022-03-14 15:03:07 +0800

    feat: add snippets

---
+++ snippets/deepClone.js
@@ -0,0 +1,28 @@
+function deepClone(obj) {
+  function isObject(o) {
+    return (typeof o === 'object' || typeof o === 'function') && o !== null
+  }
+  if (!isObject(obj)) {
+    throw new Error('非对象')
+  }
+  let isArray = Array.isArray(obj)
+  let newObj = isArray ? [...obj] : { ...obj }
+  Reflect.ownKeys(newObj).forEach(key => {
+    newObj[key] = isObject(obj[key]) ? deepClone(obj[key]) : obj[key]
+  })
+  return newObj
+}
+
+
+// 代码来源于：代码来源于 yck 的 前端面试之道-第3章-JS 基础知识点及常考面试题（二）
+
+let obj = {
+  a: [1, 2, 3],
+  b: {
+    c: 2,
+    d: 3
+  }
+}
+let newObj = deepClone(obj)
+newObj.b.c = 1
+console.log(obj.b.c) // 2
